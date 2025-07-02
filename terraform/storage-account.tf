f8754db79d67befb5d7e6bdc313604f29451717e
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-02 16:25:07 +0100

    Add container and blob for backup

--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -16,3 +16,18 @@ resource "azurerm_storage_account" "lab-stg" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_container" "container-1" {
+  name = "container-1"
+  storage_account_id = azurerm_storage_account.lab-stg.id
+}
+
+resource "azurerm_storage_blob" "blob-backup-test" {
+  name = "blob-backup-test"
+  storage_container_name = azurerm_storage_container.container-1.name
+  storage_account_name = azurerm_storage_account.lab-stg.name
+  type = "Block"
+  source_content = <<EOF
+    Hello, world
+  EOF
+}
