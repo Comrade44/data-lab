e22f8e7eb94e8703c3af9f6ca8654d33381058fa
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-02 16:09:37 +0100

    Add backup vault

---
+++ terraform/backup-vault.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "rg-backup" {
+  name     = "rg-backup"
+  location = "uksouth"
+}
+
+resource "azurerm_recovery_services_vault" "backup-vault" {
+  name                = "backup-vault"
+  resource_group_name = azurerm_resource_group.rg-backup.name
+  location            = azurerm_resource_group.rg-backup.location
+  sku                 = "Standard"
+}
