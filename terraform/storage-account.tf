3f5d8e74081a5545f688c09985d6c40e5d4a7232
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-02 16:35:51 +0100

    Add storage share

--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -18,16 +18,22 @@ resource "azurerm_storage_account" "lab-stg" {
 }
 
 resource "azurerm_storage_container" "container-1" {
-  name = "container-1"
+  name               = "container-1"
   storage_account_id = azurerm_storage_account.lab-stg.id
 }
 
 resource "azurerm_storage_blob" "blob-backup-test" {
-  name = "blob-backup-test"
+  name                   = "blob-backup-test"
   storage_container_name = azurerm_storage_container.container-1.name
-  storage_account_name = azurerm_storage_account.lab-stg.name
-  type = "Block"
-  source_content = <<EOF
+  storage_account_name   = azurerm_storage_account.lab-stg.name
+  type                   = "Block"
+  source_content         = <<EOF
     Hello, world
   EOF
+}
+
+resource "azurerm_storage_share" "container-2" {
+  name               = "container-2"
+  quota              = 2
+  storage_account_id = azurerm_storage_account.lab-stg.id
 }
