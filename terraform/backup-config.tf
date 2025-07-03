e20394544caf5ea5bb0a52bb4913691e5d7189cd
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-03 14:49:32 +0100

    Add container scope to backup instance

--- terraform/backup-config.tf
+++ terraform/backup-config.tf
@@ -18,9 +18,10 @@ resource "azurerm_data_protection_backup_policy_blob_storage" "blob-backup-polic
 }
 
 resource "azurerm_data_protection_backup_instance_blob_storage" "lab-stg-blob-policy-assignment" {
-  name               = "lab-stg-blob-policy-assignment"
-  location           = azurerm_resource_group.rg-backup.location
-  vault_id           = azurerm_data_protection_backup_vault.backup-vault.id
-  storage_account_id = azurerm_storage_account.lab-stg.id
-  backup_policy_id   = azurerm_data_protection_backup_policy_blob_storage.blob-backup-policy.id
+  name                            = "lab-stg-blob-backup"
+  location                        = azurerm_resource_group.rg-backup.location
+  vault_id                        = azurerm_data_protection_backup_vault.backup-vault.id
+  storage_account_id              = azurerm_storage_account.lab-stg.id
+  backup_policy_id                = azurerm_data_protection_backup_policy_blob_storage.blob-backup-policy.id
+  storage_account_container_names = [azurerm_storage_container.container-1.name]
 }
