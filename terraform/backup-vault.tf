5aa3bbf263c1bab89f63bcd1350c49faac6b80f1
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-03 11:24:28 +0100

    Add backup vault identity

--- terraform/backup-vault.tf
+++ terraform/backup-vault.tf
@@ -8,4 +8,7 @@ resource "azurerm_recovery_services_vault" "backup-vault" {
   resource_group_name = azurerm_resource_group.rg-backup.name
   location            = azurerm_resource_group.rg-backup.location
   sku                 = "Standard"
+  identity {
+    type = "SystemAssigned"
+  }
 }
