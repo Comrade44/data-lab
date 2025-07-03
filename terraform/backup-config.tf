d0eb8d3cc73bb99347bbb5bbfd24791a88d54451
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-03 13:28:20 +0100

    Increase backup frequency

--- terraform/backup-config.tf
+++ terraform/backup-config.tf
@@ -10,7 +10,7 @@ resource "azurerm_data_protection_backup_policy_blob_storage" "blob-backup-polic
   operational_default_retention_duration = "P1D"
   time_zone = "Coordinated Universal Time"
   vault_default_retention_duration = "P90D"
-  backup_repeating_time_intervals = [ "R/${formatdate("YYYY-MM-DD", timestamp())}T19:00:00+00:00/P1D" ]
+  backup_repeating_time_intervals = [ "R/${formatdate("YYYY-MM-DD", timestamp())}T19:00:00+00:00/P5M" ]
 
   lifecycle {
     ignore_changes = [ backup_repeating_time_intervals ]
