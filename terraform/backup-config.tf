e2bf30fbacf09b8857545f4ec44df26ad601fd7f
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-03 11:30:23 +0100

    Adding backup vault RBAC role assignment

--- terraform/backup-config.tf
+++ terraform/backup-config.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "vault-contributor" {
   scope                = azurerm_storage_account.lab-stg.id
   role_definition_name = "Storage Account Backup Contributor"
-  principal_id         = azurerm_recovery_services_vault.backup-vault.identity.principal_id
+  principal_id         = azurerm_recovery_services_vault.backup-vault.identity[0].principal_id
 }
