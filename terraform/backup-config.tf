9721f0534d0ab82ab096908e45d7f4caec5b179e
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-03 12:10:55 +0100

    Add basic backup policy

--- terraform/backup-config.tf
+++ terraform/backup-config.tf
@@ -3,3 +3,8 @@ resource "azurerm_role_assignment" "vault-contributor" {
   role_definition_name = "Storage Account Backup Contributor"
   principal_id         = azurerm_data_protection_backup_vault.backup-vault.identity[0].principal_id
 }
+
+resource "azurerm_data_protection_backup_policy_blob_storage" "blob-backup-policy" {
+  name     = "blob-backup-policy"
+  vault_id = azurerm_data_protection_backup_vault.backup-vault.id
+}
