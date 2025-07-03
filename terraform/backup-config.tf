803ad4406f4688ab29501d93e1a0d859b20b0d22
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-03 12:40:55 +0100

    Troubleshooting policy implementation

--- terraform/backup-config.tf
+++ terraform/backup-config.tf
@@ -7,6 +7,4 @@ resource "azurerm_role_assignment" "vault-contributor" {
 resource "azurerm_data_protection_backup_policy_blob_storage" "blob-backup-policy" {
   name                             = "blob-backup-policy"
   vault_id                         = azurerm_data_protection_backup_vault.backup-vault.id
-  vault_default_retention_duration = "P2D"
-  backup_repeating_time_intervals  = ["R/PT1D/2025-07-04T01:30:00"]
 }
