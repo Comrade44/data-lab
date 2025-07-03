a3e131254143274a0c0dcac6e6f5c06b3a01898a
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-03 12:13:32 +0100

    Add vault retention period

--- terraform/backup-config.tf
+++ terraform/backup-config.tf
@@ -5,6 +5,7 @@ resource "azurerm_role_assignment" "vault-contributor" {
 }
 
 resource "azurerm_data_protection_backup_policy_blob_storage" "blob-backup-policy" {
-  name     = "blob-backup-policy"
-  vault_id = azurerm_data_protection_backup_vault.backup-vault.id
+  name                             = "blob-backup-policy"
+  vault_id                         = azurerm_data_protection_backup_vault.backup-vault.id
+  vault_default_retention_duration = "P2D"
 }
