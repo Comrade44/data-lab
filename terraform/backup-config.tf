bd612fb48270acac691448ddc95e22c9b55f53e5
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-04 16:21:03 +0100

    Add file share backup

--- terraform/backup-config.tf
+++ terraform/backup-config.tf
@@ -25,3 +25,25 @@ resource "azurerm_data_protection_backup_instance_blob_storage" "lab-stg-blob-po
   backup_policy_id                = azurerm_data_protection_backup_policy_blob_storage.blob-backup-policy.id
   storage_account_container_names = [azurerm_storage_container.container-1.name]
 }
+
+resource "azurerm_backup_policy_file_share" "container-2-snapshot" {
+  name                = "container-2-snapshot"
+  resource_group_name = azurerm_resource_group.rg-backup.name
+  recovery_vault_name = azurerm_data_protection_backup_vault.backup-vault.name
+  backup {
+    frequency = "Daily"
+    time      = "23:00"
+  }
+
+  retention_daily {
+    count = 10
+  }
+}
+
+resource "azurerm_backup_protected_file_share" "container-2" {
+  resource_group_name       = azurerm_resource_group.rg-backup.name
+  recovery_vault_name       = azurerm_data_protection_backup_vault.backup-vault.name
+  source_storage_account_id = azurerm_storage_account.lab-stg.id
+  source_file_share_name    = azurerm_storage_share.container-2.name
+  backup_policy_id          = azurerm_backup_policy_file_share.container-2-snapshot.id
+}
