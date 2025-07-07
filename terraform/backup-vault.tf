211aec58ed8c8d53697c2c71536289c8961206ab
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-07 11:48:42 +0100

    Disable soft delete on vault

--- terraform/backup-vault.tf
+++ terraform/backup-vault.tf
@@ -20,6 +20,7 @@ resource "azurerm_recovery_services_vault" "files-vault" {
   location            = "uksouth"
   resource_group_name = azurerm_resource_group.rg-backup.name
   sku                 = "Standard"
+  soft_delete_enabled = false
   identity {
     type = "SystemAssigned"
   }
