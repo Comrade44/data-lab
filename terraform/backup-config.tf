4e067504cd2b5b48042f8f83361d815752719c38
Richard McGinn <richard.mcginn@blakyaks.com>
2025-07-07 09:58:55 +0100

    Re-add file share

--- terraform/backup-config.tf
+++ terraform/backup-config.tf
@@ -47,10 +47,10 @@ resource "azurerm_backup_policy_file_share" "container-2-snapshot" {
   }
 }
 
-#resource "azurerm_backup_protected_file_share" "container-2" {
-#  resource_group_name       = azurerm_resource_group.rg-backup.name
-#  recovery_vault_name       = azurerm_recovery_services_vault.files-vault.name
-#  source_storage_account_id = azurerm_storage_account.lab-stg.id
-#  source_file_share_name    = azurerm_storage_share.container-2.name
-#  backup_policy_id          = azurerm_backup_policy_file_share.container-2-snapshot.id
-#}
+resource "azurerm_backup_protected_file_share" "container-2" {
+  resource_group_name       = azurerm_resource_group.rg-backup.name
+  recovery_vault_name       = azurerm_recovery_services_vault.files-vault.name
+  source_storage_account_id = azurerm_storage_account.lab-stg.id
+  source_file_share_name    = azurerm_storage_share.container-2.name
+  backup_policy_id          = azurerm_backup_policy_file_share.container-2-snapshot.id
+}
